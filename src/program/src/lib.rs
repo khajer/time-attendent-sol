ee462b8a1bc50552d7161c16b5edc130bb67a668
kha <itsaraK>
2022-10-17 00:14:17 +0700

    rename flow rust instrctuion

--- src/program/src/lib.rs
+++ src/program/src/lib.rs
@@ -12,8 +12,8 @@ entrypoint!(process_instruction);
 
 // Program entrypoint's implementation
 pub fn process_instruction(
-    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
-    accounts: &[AccountInfo], // The account to say hello to
+    _program_id: &Pubkey, // Public key of the account the hello world program was loaded into
+    _accounts: &[AccountInfo], // The account to say hello to
     _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
 ) -> ProgramResult {
     msg!("Hello World Rust program entrypoint");    
