dfdb3c66fd77a13806dee6c3a5888248174d5894
kha <itsaraK>
2022-10-17 01:16:43 +0700

    check owner program

--- src/program/src/lib.rs
+++ src/program/src/lib.rs
@@ -1,9 +1,11 @@
 
+
 use solana_program::{
-    account_info::AccountInfo,
+    account_info::{next_account_info, AccountInfo},
     entrypoint,
     entrypoint::ProgramResult,
     msg,
+    program_error::ProgramError,
     pubkey::Pubkey,
 };
 
@@ -12,11 +14,19 @@ entrypoint!(process_instruction);
 
 // Program entrypoint's implementation
 pub fn process_instruction(
-    _program_id: &Pubkey, // Public key of the account the hello world program was loaded into
-    _accounts: &[AccountInfo], // The account to say hello to
+    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
+    accounts: &[AccountInfo], // The account to say hello to
     _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
 ) -> ProgramResult {
     msg!("Hello World Rust program entrypoint");    
+    let accounts_iter = &mut accounts.iter();
+    
+    let account = next_account_info(accounts_iter)?;
+
+    if &*account.owner != program_id {
+        msg!("Account does not correct");
+        return Err(ProgramError::IncorrectProgramId);
+    }
 
     Ok(())
 }
